865c1044f2f35f8285867ea502b4ed9dab5993b3
Simon Stubenvoll <simon.stubenvoll@gmail.com>
2020-12-20 20:19:16 +0100

    add @11ty/eleventy-navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const pluginNavigation = require("@11ty/eleventy-navigation");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginNavigation);
+
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("img");
 };
