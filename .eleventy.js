e8af09f590d744615c1e22e407ee21d35c220c8e
Simon Stubenvoll <simon.stubenvoll@gmail.com>
2021-01-24 16:22:58 +0100

    add readableDate filter

--- .eleventy.js
+++ .eleventy.js
@@ -8,15 +8,27 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("js");
 
+  // Collections
   eleventyConfig.addCollection("jobs", function (collection) {
     return collection
       .getFilteredByGlob(globs.jobs)
       .sort((a, b) => b.date - a.date);
   });
+
   eleventyConfig.addCollection("posts", function (collection) {
     return collection
       .getFilteredByGlob(globs.posts)
       .sort((a, b) => b.date - a.date);
+  });
+
+  // Filters
+  eleventyConfig.addFilter("readableDate", function (iso) {
+    const date = new Date(iso);
+    const MM = date.getMonth() + 1;
+    const YYYY = date.getFullYear();
+
+    MM = MM.toLocaleString(undefined, { minimumIntegerDigits: 2 });
+    return `${MM}/${YYYY}`;
   });
 
   eleventyConfig.addFilter("first", function (array) {
